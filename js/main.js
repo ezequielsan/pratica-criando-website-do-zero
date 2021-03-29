3b2d8a0e464e9598f38ca360fa066a82f69da5f8
Ezequiel Santos <ezequiel.mozart.2020@gmail.com>
2021-03-29 20:39:35 -0300

    site finalizado

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('#slider-principal').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  mobileFirst: true,
+});
+
+
